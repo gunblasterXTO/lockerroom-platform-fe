4af27515907588fd665d88cb4ce0157a2e5c02de
gunblasterXTO <naufal.abdila@outlook.com>
2024-03-31 10:52:28 +0700

    chore: update project structure

---
+++ lib/modules/auth/controllers/login.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  late TextEditingController usernameController, passwordController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    usernameController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void onClose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.onClose();
+  }
+
+  String? validateUsername(String value) {
+    if (value.isEmpty || value.trim().isEmpty) {
+      return "Fill in username";
+    }
+    return null;
+  }
+
+  String? validatePassword(String value) {
+    if (value.length < 6) {
+      return "Password should be of minimum 6 characters";
+    }
+    return null;
+  }
+
+  void checkLogin() {
+    final isValid = loginFormKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    loginFormKey.currentState!.save();
+  }
+}
---
+++ lib/modules/auth/views/login.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lockerroom/common/helper/ui_helper.dart';
+import 'package:lockerroom/common/widgets/button.dart';
+import 'package:lockerroom/common/widgets/text_form.dart';
+import 'package:lockerroom/modules/auth/controllers/login.dart';
+
+class LoginPage extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(
+              top: 16,
+              left: UIConst.pagePaddingHorizontal,
+              right: UIConst.pagePaddingHorizontal),
+          width: context.width,
+          height: context.height,
+          child: Form(
+            key: controller.loginFormKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                Text(
+                  'Welcome to Lockerroom!',
+                  style: context.textTheme.displayLarge,
+                ),
+                SizedBox(
+                  height: UIConst.bigSizedBoxHeight,
+                ),
+                CustomInputText(
+                  required: true,
+                  controller: controller.usernameController,
+                  // onSaved: (value) => controller.username = value!,
+                  onSaved: (value) => {},
+                  validator: (value) => controller.validateUsername(value!),
+                  hint: 'username',
+                  obsecure: false,
+                ),
+                SizedBox(
+                  height: UIConst.smallSizedBoxHeight,
+                ),
+                CustomInputText(
+                    required: true,
+                    controller: controller.passwordController,
+                    // onSaved: (value) => controller.password = value!,
+                    onSaved: (value) => {},
+                    validator: (value) => controller.validatePassword(value!),
+                    hint: 'password',
+                    obsecure: true),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Text('Forget password?')),
+                SizedBox(
+                  height: UIConst.smallSizedBoxHeight,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: CustomElevatedButton(
+                            text: 'Login',
+                            onPressed: () => controller.checkLogin())),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
