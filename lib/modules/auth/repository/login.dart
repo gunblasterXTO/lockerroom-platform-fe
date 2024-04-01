7bada7cd8d5f75ea4fe234708d38e73213a2ec92
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-01 15:19:35 +0700

    feat: simplify model implementation

--- lib/modules/auth/repository/login.dart
+++ lib/modules/auth/repository/login.dart
@@ -2,35 +2,24 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-import '../../../common/helper/models.dart';
 import '../model/login.dart';
 
 class RemoteLoginRepository {
   final String url;
   RemoteLoginRepository({required this.url});
 
-  Future<Map<String, dynamic>> login(String username, String password) async {
+  Future<LoginResponse> login(String username, String password) async {
     final reqJson =
         LoginRequest(username: username, password: password).toJson();
-    print(reqJson);
-    try {
-      var response = await http.post(Uri.parse(url),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode(reqJson));
-      final responseBody = jsonDecode(response.body);
-      if (response.statusCode == 200) {
-        final successResp = LoginResponse.fromJson(
-            PostSuccessResponse(data: responseBody['data']).toJson());
-        return successResp.toJson();
-      } else {
-        final failResp = FailResponse(
-            status: responseBody['status'], detail: responseBody['detail']);
-        return failResp.toJson();
-      }
-    } catch (e) {
-      throw ('Error while getting data: $e');
-    }
+
+    var response = await http.post(Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(reqJson));
+    final Map<String, dynamic> bodyResp = jsonDecode(response.body);
+    final resp = LoginResponse.fromJson(response.statusCode, bodyResp);
+
+    return resp;
   }
 }
