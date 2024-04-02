52ecce3b6dd92805b62b2c9e87fe99f1a274c3f2
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-02 15:09:55 +0700

    feat: create welcome page

---
+++ lib/modules/auth/views/welcome.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lockerroom/common/styles/button.dart';
+import 'package:lockerroom/modules/auth/bindings/login.dart';
+import 'package:lockerroom/modules/home/home.dart';
+
+import '../../../common/helper/ui_helper.dart';
+import '../../../common/widgets/button.dart';
+import 'login.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/welcome.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Give fade effect on background image
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.transparent, Colors.black.withOpacity(1.0)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          // Action button
+          Positioned(
+            bottom: 120,
+            left: UIConst.pagePaddingHorizontal,
+            right: UIConst.pagePaddingHorizontal,
+            child: CustomElevatedButton(
+              text: 'Login',
+              style: primaryElevatedButton,
+              onPressed: () =>
+                  Get.to(() => const LoginPage(), binding: LoginBinding()),
+            ),
+          ),
+          Positioned(
+            bottom: 60,
+            left: UIConst.pagePaddingHorizontal,
+            right: UIConst.pagePaddingHorizontal,
+            child: CustomElevatedButton(
+              text: 'Register',
+              style: secondaryElevatedButton,
+              onPressed: () {
+                print("ke hit");
+              },
+            ),
+          ),
+          // Continue as guest
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: CustomTextButton(
+                text: 'Continue as guest',
+                textStyle: secondaryTextButton,
+                onTap: () => Get.to(() => const HomePage()),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
