f7756695246dcba0aaea0363dacfcc6a7e1428d6
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-03 06:56:59 +0700

    chore: rename file

--- lib/common/widgets/form.dart
+++ lib/common/widgets/form.dart
@@ -8,17 +8,20 @@ class CustomInputText extends StatelessWidget {
   final String? Function(String?)? validator;
   final String hint;
   final bool obsecure;
+  final Icon? prefixIcon;
   final double? width;
   final double? height;
-  const CustomInputText(
-      {super.key,
-      required this.required,
-      required this.controller,
-      required this.validator,
-      required this.hint,
-      required this.obsecure,
-      this.width,
-      this.height});
+  const CustomInputText({
+    super.key,
+    required this.required,
+    required this.controller,
+    required this.validator,
+    required this.hint,
+    required this.obsecure,
+    this.prefixIcon,
+    this.width,
+    this.height,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +33,17 @@ class CustomInputText extends StatelessWidget {
         validator: validator,
         obscureText: obsecure,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(15),
+          contentPadding:
+              EdgeInsets.all(UIConst.textFormFieldDecorationPadding),
+          prefixIcon: prefixIcon,
           hintText: hint,
-          hintStyle: context.textTheme.displaySmall,
+          hintStyle: context.textTheme.titleSmall,
           border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
-              borderRadius:
-                  BorderRadius.circular(UIConst.circularBorderRadius)),
+            borderSide: const BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(UIConst.circularBorderRadius),
+          ),
+          errorMaxLines: 1,
+          errorStyle: const TextStyle(height: 0.8),
         ),
       ),
     );
