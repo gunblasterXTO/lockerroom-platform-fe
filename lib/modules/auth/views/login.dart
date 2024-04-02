90d822021051d861e105ca6008898ec4e90c6ce7
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-03 06:57:35 +0700

    feat: rework login page

--- lib/modules/auth/views/login.dart
+++ lib/modules/auth/views/login.dart
@@ -1,70 +1,125 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lockerroom/common/helper/ui_helper.dart';
-import 'package:lockerroom/common/widgets/button.dart';
-import 'package:lockerroom/common/widgets/text_form.dart';
+import 'package:lockerroom/common/styles/button.dart';
+import 'package:lockerroom/common/widgets/form.dart';
 import 'package:lockerroom/modules/auth/controllers/login.dart';
+import 'package:lockerroom/modules/auth/views/register.dart';
 
 class LoginPage extends GetView<LoginController> {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_rounded),
+        ),
+      ),
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(
-              top: 16,
-              left: UIConst.pagePaddingHorizontal,
-              right: UIConst.pagePaddingHorizontal),
-          width: context.width,
-          height: context.height,
-          child: Form(
-            key: controller.loginFormKey,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Column(
-              children: [
-                Text(
-                  'Welcome to Lockerroom!',
-                  style: context.textTheme.displayLarge,
-                ),
-                SizedBox(
-                  height: UIConst.bigSizedBoxHeight,
-                ),
-                CustomInputText(
-                  required: true,
-                  controller: controller.usernameController,
-                  validator: (value) => controller.validateUsername(value!),
-                  hint: 'username',
-                  obsecure: false,
-                ),
-                SizedBox(
-                  height: UIConst.smallSizedBoxHeight,
-                ),
-                CustomInputText(
-                    required: true,
-                    controller: controller.passwordController,
-                    validator: (value) => controller.validatePassword(value!),
-                    hint: 'password',
-                    obsecure: true),
-                Align(
-                    alignment: Alignment.centerRight,
-                    child: Text('Forget password?')),
-                SizedBox(
-                  height: UIConst.smallSizedBoxHeight,
-                ),
-                Row(
+        child: Padding(
+          padding: EdgeInsets.all(UIConst.pagePaddingHorizontal),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello, welcome back! \u{1F3C0}',
+                style: context.textTheme.headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: UIConst.smallSizedBoxHeight,
+              ),
+              Text(
+                'Happy to see you again, ready to ball?',
+                style: context.textTheme.bodySmall,
+              ),
+              SizedBox(height: UIConst.bigSizedBoxHeight),
+              //Form
+              Form(
+                key: controller.loginFormKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
                   children: [
-                    Expanded(
-                        child: CustomElevatedButton(
-                            text: 'Login',
-                            onPressed: () {
-                              controller.checkLogin(
-                                  controller.usernameController.text,
-                                  controller.passwordController.text);
-                            })),
+                    CustomInputText(
+                      controller: controller.usernameController,
+                      height: UIConst.formHeight,
+                      hint: 'Enter your username',
+                      obsecure: false,
+                      prefixIcon: const Icon(Icons.person),
+                      required: true,
+                      validator: (value) => controller.validateUsername(value!),
+                    ),
+                    SizedBox(height: UIConst.formSizedBoxHeight),
+                    CustomInputText(
+                      controller: controller.passwordController,
+                      height: UIConst.formHeight,
+                      hint: 'Enter your password',
+                      obsecure: true,
+                      prefixIcon: const Icon(Icons.lock),
+                      required: true,
+                      validator: (value) => controller.validatePassword(value!),
+                    ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  'Forget password?',
+                  style: context.textTheme.bodyMedium,
+                ),
+              ),
+              SizedBox(
+                height: UIConst.formSizedBoxHeight,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: primaryElevatedButton.copyWith(),
+                      onPressed: () {
+                        controller.checkLogin(
+                          controller.usernameController.text,
+                          controller.passwordController.text,
+                        );
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: UIConst.formSizedBoxHeight,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Don't have an account? ",
+                        style: context.textTheme.bodyMedium,
+                      ),
+                      TextSpan(
+                        text: "Register",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Get.to(() => const RegisterPage());
+                          },
+                        style: context.textTheme.bodyMedium!.copyWith(
+                          color: UIConst.lightPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
