9bb072e80725275474b656a48c18b6708fc72dad
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-03 06:56:10 +0700

    feat: add more ui helper

--- lib/common/helper/ui_helper.dart
+++ lib/common/helper/ui_helper.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
+
 class UIConst {
   UIConst._();
 
+  // Color
+  static Color lightPrimaryColor = const Color.fromRGBO(199, 20, 26, 1);
+  static Color lightSecondaryColor = Colors.white;
+
+  // Size
   static double pagePaddingHorizontal = 20.0;
   static double circularBorderRadius = 10.0;
-  static double bigSizedBoxHeight = 60.0;
-  static double smallSizedBoxHeight = 15.0;
+  static double bigSizedBoxHeight = 50.0;
+  static double smallSizedBoxHeight = 5.0;
+  static double buttonSizedBoxHeight = 15.0;
+  static double formSizedBoxHeight = 10.0;
+  static double formHeight = 75.0;
+  static double textFormFieldDecorationPadding = 15.0;
 }
