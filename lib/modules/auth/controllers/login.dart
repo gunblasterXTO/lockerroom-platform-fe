e2923dc296625833c8aa1e7601624d164893d930
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-02 15:13:31 +0700

    feat: login controller checkLogin update

--- lib/modules/auth/controllers/login.dart
+++ lib/modules/auth/controllers/login.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lockerroom/modules/auth/model/login.dart';
+import 'package:lockerroom/modules/auth/repository/login.dart';
+import 'package:lockerroom/modules/home/home.dart';
 
 class LoginController extends GetxController {
   final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  final RemoteLoginRepository _loginRepository =
+      RemoteLoginRepository(url: 'http://10.0.2.2:80/v1/auth/login');
   late TextEditingController usernameController, passwordController;
 
   @override
@@ -33,11 +38,25 @@ class LoginController extends GetxController {
     return null;
   }
 
-  void checkLogin() {
+  void checkLogin(String username, String password) async {
     final isValid = loginFormKey.currentState!.validate();
     if (!isValid) {
       return;
     }
-    loginFormKey.currentState!.save();
+    LoginResponse response = await _loginRepository.login(username, password);
+    switch (response.statusCode) {
+      case 200:
+        toHomePage();
+      default:
+        Get.snackbar(
+          'Login fail',
+          response.fail!.detail,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+    }
+  }
+
+  void toHomePage() {
+    Get.to(() => const HomePage());
   }
 }
